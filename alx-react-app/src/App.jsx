b8744f6ce9d491ed985f180ecebf1ad6a29e0bba
BrightAquilla <aquillabright@gmail.com>
2026-01-18 22:06:29 +0300

    react header main footer commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import WelcomeMessage from './assets/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
